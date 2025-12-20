b80289539ae9d63eb977346642eea5483c008f3a
Nabin Sharma (Ns329) <nabinsharma816@gmail.com>
2025-12-20 06:52:52 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-9799584158-38330",
+  "appId": "1:692115249356:web:ad279c38079fe8fe488a69",
+  "apiKey": "AIzaSyAkadPAJcjhVTf4v2kKOezLLJ51Q-P57vE",
+  "authDomain": "studio-9799584158-38330.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "692115249356"
+};
