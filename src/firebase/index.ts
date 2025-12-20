53986f5407400ebafa2466034a1e152f6a308e41
Nabin Sharma (Ns329) <nabinsharma816@gmail.com>
2025-12-20 15:42:49 +0000

    Application error: a client-side exception has occurred while loading gs

--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -8,7 +8,8 @@ import { getFirestore } from 'firebase/firestore'
 // IMPORTANT: DO NOT MODIFY THIS FUNCTION
 export function initializeFirebase() {
   if (getApps().length) {
-    return getSdks(getApp());
+    const app = getApp();
+    return getSdks(app);
   }
 
   // This configuration is used for Vercel deployments and other environments
@@ -17,12 +18,11 @@ export function initializeFirebase() {
       apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
       authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
       projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-      storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
       messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
       appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
   };
 
-  // Check if the essential prod vars are set. storageBucket is optional.
+  // Check if the essential prod vars are set.
   const useProdConfig = 
     prodFirebaseConfig.apiKey &&
     prodFirebaseConfig.authDomain &&
@@ -46,6 +46,5 @@ export * from './client-provider';
 export * from './firestore/use-collection';
 export * from './firestore/use-doc';
 export * from './non-blocking-updates';
-export * from './non-blocking-login';
 export * from './errors';
 export * from './error-emitter';
